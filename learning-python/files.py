13774c8187158fc755545f1fe8df8494e7e9b207
gabesayshi <gabesayshi@gmail.com>
2022-10-13 00:15:39 -0400

    finished file script

--- learning-python/files.py
+++ learning-python/files.py
@@ -3,3 +3,21 @@ def main():
     for i in range(10):
         myFile.write("This is new text\n")
     myFile.close()
+
+    myFile = open("textfile.txt", "a+")
+    for i in range(10):
+        myFile.write("This is new text\n")
+    myFile.close()
+
+def backup():
+    myFile = open("textfile.txt", "r")
+    if myFile.mode == 'r':
+        content = myFile.read()
+        print(content)
+        
+        fileLines = myFile.readlines() #sends lines to a list
+        for i in fileLines:
+            print(i)
+
+main()
+backup()
